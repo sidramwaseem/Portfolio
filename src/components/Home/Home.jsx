4b5d3625f1fa9da9bff52da7e6d717bf77b0e91a
Sidra Waseem <sidraw24@gmail.com>
2022-09-01 21:59:48 +0500

    Complete homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -37,73 +37,64 @@ function Home() {
         </div>
       </nav>
       <section id="home">
-        <aside className="social responsive-social">
-          <ul>
-            <li>
-              <a
-                href="https://twitter.com/sidramwaseem"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Twitter <i class="fa-brands fa-twitter"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/sidramwaseem"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Github <i class="fa-brands fa-github"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.linkedin.com/in/sidramwaseem/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Linkedin <i class="fa-brands fa-linkedin-in"></i>
-              </a>
-            </li>
-          </ul>
-        </aside>
+      <main className="main-homepage">
 
-        <article className="personal-info">
-          <blockquote className="name">
-            <p>
-              Hello, <span className="cyan">I'm</span>{" "}
-            </p>
-            <h4 className="wordCarousel">
-              <div>
-                <ul className="flip">
-                  <li>Mentee</li>
-                  <li>Web Developer</li>
-                  <li>Coder</li>
-                  <li>Frontend Developer</li>
-                  <li>Sidra Waseem</li>
-                </ul>
-              </div>
-            </h4>
-          </blockquote>
-          <div className="para">
-            <p>
-              I am an ambitious and passionate developer whose everyday mission
-              is to take a step further with her skills and learn new things.
-              The enthusiasm I've for my work keeps me committed to
-              self-development so I can achieve better results.
-            </p>
+      <article className="personal-info">
+        <blockquote className="name">
+          <p>
+            Hello, <span className="cyan">I'm</span>{" "}
+          </p>
+          <h4 className="wordCarousel">
+            <div>
+              <ul className="flip">
+                <li>Mentee</li>
+                <li>Web Developer</li>
+                <li>Coder</li>
+                <li>Frontend Developer</li>
+                <li>Sidra Waseem</li>
+              </ul>
+            </div>
+          </h4>
+        </blockquote>
+        <div className="para">
+          <p>
+            I am an ambitious and passionate developer whose everyday mission
+            is to take a step further with her skills and learn new things.
+            The enthusiasm I've for my work keeps me committed to
+            self-development so I can achieve better results.
+          </p>
 
-            <p className="margin">
-              However, there is a high probability this developer is still
-              googling how to center a <code>div</code>.
-            </p>
-          </div>
-          <button className="btn">Download CV</button>
-        </article>
-        <figure className="profile-figure">
-          <img src={profileImg} alt="Profile" className="profile-img" />
-        </figure>
+          <p className="margin">
+            However, there is a high probability this developer is still
+            googling how to center a <code>div</code>.
+          </p>
+        </div>
+        <button className="btn" > <a href="../../assets/CV.pdf" title="CV" download>Download CV</a></button>
+      </article>
+      <figure className="profile-figure">
+        <img src={profileImg} alt="Profile" className="profile-img" />
+      </figure>
+      </main>
+      <div className="socials-list">
+      <ul className="social">
+      <li>Check Out My:</li>
+        <li>
+          <a href="h">
+            <i class="fab fa-twitter" aria-hidden="true"></i>
+          </a>
+        </li>
+        <li>
+          <a href="h">
+            <i class="fab fa-linkedin" aria-hidden="true"></i>
+          </a>
+        </li>
+        <li>
+          <a href="h">
+            <i class="fab fa-github" aria-hidden="true"></i>
+          </a>
+        </li>
+      </ul>
+      </div>
       </section>
     </header>
   );
