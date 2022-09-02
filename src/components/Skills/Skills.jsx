c900c7da48bfe6a80a7d33a0162ec60a55aa9769
Sidra Waseem <sidraw24@gmail.com>
2022-09-03 00:02:09 +0500

    format skill component

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
-import tags from './tags.js';
-import './Skills.css'
+import React from "react";
+import tags from "./tags.js";
+import "./Skills.css";
 
 function Skills() {
-
-    const tagElement = tags.map(tag => {
-        return  <a href={tag.href} target="_blank" rel="noreferrer"><span style={{color:tag.color,backgroundColor:tag.backgroundColor,cursor: tag.href?"pointer":"not-allowed"}} className="tag">{tag.name}</span></a>
-    })
+  const tagElement = tags.map((tag) => {
+    return (
+      <a href={tag.href} target="_blank" rel="noreferrer">
+        <span
+          style={{
+            color: tag.color,
+            backgroundColor: tag.backgroundColor,
+            cursor: tag.href ? "pointer" : "not-allowed",
+          }}
+          className="tag"
+        >
+          {tag.name}
+        </span>
+      </a>
+    );
+  });
   return (
     <section id="skills">
-    <article className='skill-header'>
-    <h2>My <span className='cyan'>Skillset</span></h2>
-    <p>Fingers crossed for the colorless tags to get their entitled colors soon.</p>
-    </article>
-    <main className='tags-container'>
-    {tagElement}
-    </main>
+      <article className="skill-header">
+        <h2>
+          My <span className="cyan">Skillset</span>
+        </h2>
+        <p>
+          Fingers crossed for the colorless tags to get their entitled colors
+          soon.
+        </p>
+      </article>
+      <main className="tags-container">{tagElement}</main>
     </section>
-  )
+  );
 }
 
-export default Skills
+export default Skills;
