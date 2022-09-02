494a62876436cd3cc7e175ec8f92d7030fe686a1
Sidra Waseem <sidraw24@gmail.com>
2022-09-02 23:02:25 +0500

    create data file for tags

---
+++ src/components/Skills/tags.js
@@ -0,0 +1,187 @@
+const tags=[
+    {
+        name:"HTML",
+        backgroundColor:"#e34c26",
+        color:"#fff"
+    },
+    {
+        name:"CSS",
+        backgroundColor:"#264de4",
+        color:"#ebebeb",
+    },
+    {
+        name:"JavaScript",
+        backgroundColor:"#f0db4f",
+        color:"#323330",
+    },
+    {
+        name:"ECMAScript6",
+        backgroundColor:"#f0db4f",
+        color:"#323330",
+    },
+    {
+        name:"iOS",
+        
+    },
+    {
+        name:"Angular",
+
+    },
+    {
+        name:"React",
+        backgroundColor:"#000",
+        color:"#02D6FF",
+    },
+    {
+        name:"React Native",
+
+    },
+    {
+        name:"Vue",
+        
+    },
+    {
+        name:"AWS",
+        
+    },
+    {
+        name:"jQuery",
+        backgroundColor:"#0868ac",
+        color:"#fff",
+    },
+    {
+        name:"NodeJS",
+        backgroundColor:"#3c873a",
+        color:"#303030",
+    },
+    {
+        name:"TypeScript",
+
+    },
+    {
+        name:"Android",
+        
+    },
+    {
+        name:"Redux",
+    },
+    {
+        name:"Linux",
+        
+    },
+    {
+        name:"DevOps",
+       
+    },
+    {
+        name:"Python",
+        backgroundColor:"#4b8bbe",
+        color:"#ffd43b",
+    },
+    {
+        name:"PHP",
+        backgroundColor:"#474a8a",
+        color:"#000",
+    },
+    {
+        name:"C++",
+        backgroundColor:"#004482",
+        color:"#FFFFFF",
+    },
+    {
+        name:"C#",
+        backgroundColor:"#682876",
+        color:"#FFFFFF",
+    },
+    {
+        name:"Dot Net",
+   
+    },
+    {
+        name:"PWA",
+      
+    },
+    {
+        name:"SAAS",
+        backgroundColor:"#cd6799",
+        color:"#fff",
+    },
+    {
+        name:"Bootstrap",
+        backgroundColor:"#563d7c",
+        color:"#fff",
+    },
+    {
+        name:"MongoDB",
+
+    },
+    {
+        name:"ExpressJS",
+    
+    },
+    {
+        name:"Git",
+        backgroundColor:"#f1502f",
+        color:"#3e2c00",
+    },
+    {
+        name:"GitHub",
+        backgroundColor:"#211f1f",
+        color:"#fff",
+    },
+    {
+        name:"SEO",
+        
+    },
+    {
+        name:"SQL",
+        backgroundColor:"#00758f",
+        color:"#f29111",
+    },
+    {
+        name:"Unity",
+
+    },
+    {
+        name:"REST / RESTful",
+
+    },
+    {
+        name:"SQL",
+        backgroundColor:"#00758f",
+        color:"#f29111",
+    },
+    {
+        name:"VS Code",
+        backgroundColor:"#0078d7",
+        color:"#fff",
+    },
+    {
+        name:"Visual Code",
+        backgroundColor:"#6B33AE",
+        color:"#fff",
+    },
+    {
+        name:"Atom",
+        backgroundColor:"#60B57E",
+        color:"#FFFDFB",
+    },
+    {
+        name:"Netlify",
+        backgroundColor:"#3FA7BE",
+        color:"#061820",
+    },
+    {
+        name:"Heroku",
+
+    },
+    {
+        name:"Firebase",
+
+    },
+
+
+]
+
+
+export default tags;
