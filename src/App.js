e79735b8cbc129ab62c272a6be37f1b3f45f004b
Sidra Waseem <sidraw24@gmail.com>
2022-09-02 23:01:41 +0500

    create map function for tags

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './components/Home/Home.jsx';
+import Skills from './components/Skills/Skills.jsx';
 import './App.css';
 
 function App() {
   return (
     <main>
       <Home/>
+      <Skills/>
     </main>
   )
 }
