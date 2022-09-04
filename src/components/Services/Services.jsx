203e943088f2c2535befc589b1385f3f4a52fd2f
Sidra Waseem <sidraw24@gmail.com>
2022-09-04 20:10:51 +0500

    update

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import './Services.css;'
+import "../../script.js";
+
+function Services() {
+  return (
+    <section id="services">
+    <blockquote className="service-header">
+    <h2>Now let's talk about what I <span className="cyan">can do </span>for you!</h2>
+    </blockquote>
+      <div class="all">
+        <div class="lefter">
+          <div class="text">Hosting</div>
+        </div>
+        <div class="left">
+          <div class="text">Web Design</div>
+        </div>
+        <div class="center">
+          <div class="explainer">
+            <span>Hover me</span>
+          </div>
+          <div class="text">Frontend Development</div>
+        </div>
+        <div class="right">
+          <div class="text">Backend Development</div>
+        </div>
+        <div class="righter">
+          <div class="text">SEO</div>
+        </div>
+      </div>
+
+      <a href="https://jouanmarcel.com" target="_blank" class="ref" rel="noreferrer">
+        🔗 Jouan Marcel
+      </a>
+    </section>
+  );
+}
+
+export default Services;
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import tags from "./tags.js";
 import "./Skills.css";
-import "../../script.js"
+import "../../script.js";
 
 function Skills() {
  
