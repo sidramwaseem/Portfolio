a1fef79f5f1e97f79b59265ab8c45b4bf7c4a319
Sidra Waseem <sidraw24@gmail.com>
2022-09-04 20:10:26 +0500

    Update App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './components/Home/Home.jsx';
 import Skills from './components/Skills/Skills.jsx';
+import Services from './components/Services/Services.jsx';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <main>
       <Home/>
       <Skills/>
+      <Services/>
     </main>
   )
 }
