095e19d9d2c834916550f499fbb65a24f71d1f16
Sidra Waseem <sidraw24@gmail.com>
2022-09-05 12:35:28 +0500

    coded project.js

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -7,7 +7,52 @@ const projects = [
         heading: 'Tenzies Game',
         demo:'https://tenzie-scrimba.netlify.app/',
         repo:'https://github.com/sidramwaseem/Tenzies',
-    }
+    },
+    {
+        image:'Advice Generator.png',
+        tag1: 'HTML',
+        tag2: 'CSS',
+        tag3: 'React',
+        heading: 'Advice Generator',
+        demo:'https://advicegenerator-react.netlify.app/',
+        repo:'https://github.com/sidramwaseem/Advice-Generator',
+    },
+    {
+        image:'Abstract Webpage.png',
+        tag1: 'HTML',
+        tag2: 'CSS',
+        tag3: 'BOOTSTRAP',
+        heading: 'Abstract Webpage',
+        demo:'https://abstract-webpage.netlify.app/',
+        repo:'https://github.com/sidramwaseem/Abstract-Webpage',
+    },
+    {
+        image:'Grade Calculator.png',
+        tag1: 'HTML',
+        tag2: 'BOOTSTRAP',
+        tag3: 'JAVASCRIPT',
+        heading: 'Grade Calculator',
+        demo:'https://grade-calculator-js.netlify.app/',
+        repo:'https://github.com/sidramwaseem/Grade-Calculator-JS',
+    },
+    {
+        image:'Interactive Rating Component.png',
+        tag1: 'HTML',
+        tag2: 'CSS',
+        tag3: 'JAVASCRIPT',
+        heading: 'Interactive Rating Component',
+        demo:'https://interactive-rating-component-frontendmentor.netlify.app/',
+        repo:'https://github.com/sidramwaseem/Interactive-Rating-Component',
+    },
+    {
+        image:'Testimonials-grid-section.png',
+        tag1: 'HTML',
+        tag2: 'CSS',
+        tag3:'CSS Grid',
+        heading: 'Testimonials-grid-section',
+        demo:'https://testimonials-section-frontend.netlify.app/',
+        repo:'https://github.com/sidramwaseem/Testimonials-grid-section',
+    },
 ]
 
 export default projects;
