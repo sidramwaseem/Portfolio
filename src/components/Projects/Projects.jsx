249f47a510319f42378420522f0d9a836d298a3c
Sidra Waseem <sidraw24@gmail.com>
2022-09-05 12:35:38 +0500

    update project component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import "./Projects.css";
-// import imgport from "../../assets/project images/Tenzies.png";
 import projects from "./projects.js";
+import "../../script.js";
 
 function Projects() {
   const projectElement = projects.map((project, index) => {
     return (
-      <div class="portfolio-grid" key={index}>
-        <figure class="project-fig">
-          <img src={`../../assets/images/${project.image}`} alt="project" />
+      <div className="portfolio-grid reveal" key={index}>
+        <figure className="project-fig">
+          <img
+            src={require(`../../assets/images/${project.image}`)}
+            alt="project"
+          />
           <figcaption>
             #{project.tag1} #{project.tag2} #{project.tag3}
           </figcaption>
         </figure>
         <h3>{project.heading}</h3>
-        <div class="buttons">
+        <div className="buttons">
           <a
-            class="demo-btn button"
+            className="demo-btn button"
             target="_blank"
             href={project.demo}
             rel="noreferrer"
@@ -25,7 +28,7 @@ function Projects() {
           </a>
 
           <a
-            class="code-btn button"
+            className="code-btn button"
             target="_blank"
             href={project.repo}
             rel="noreferrer"
@@ -38,10 +41,20 @@ function Projects() {
   });
   return (
     <section id="projects">
-      <h1 class="portfolio-heading">
+      <h1 class="portfolio-heading reveal">
         My <span className="cyan">Projects</span>.
       </h1>
       <div class="portfolio--grid-container">{projectElement}</div>
+      <div className="btn-wrapper">
+        <a
+          className="button"
+          href="https://github.com/sidramwaseem"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check Out all projects <i class="fa-solid fa-arrow-right"></i>
+        </a>
+      </div>
     </section>
   );
 }
