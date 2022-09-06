456b75b24a626ba8fafdc8df9c3807b733fb5319
Sidra Waseem <sidraw24@gmail.com>
2022-09-06 13:09:39 +0500

    create contact component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Contact.css';
+
+function Contact() {
+  return (
+    <section id="contact">
+    <h2>Let's <span className='cyan'>create</span> something cool together.</h2>
+    </section>
+  )
+}
+
+export default Contact
