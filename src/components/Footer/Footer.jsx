4002f07fc4e422cd6d384a49d831c45d50361f13
Sidra Waseem <sidraw24@gmail.com>
2022-09-07 14:30:53 +0500

    create footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Made with <i class="fa-solid fa-heart"></i> by{" "}
+        <a
+          href="https://github.com/sidramwaseem"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Sidra
+        </a>
+        - 2022
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
