1ae041fe3acfe0a37e9ec2a3344d509af1ef9770
Sidra Waseem <sidraw24@gmail.com>
2022-09-27 11:05:50 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import Home from './components/Home/Home.jsx';
-import Skills from './components/Skills/Skills.jsx';
-import Services from './components/Services/Services.jsx';
-import Projects from './components/Projects/Projects.jsx';
-import Contact from './components/Contact/Contact.jsx';
-import Footer from './components/Footer/Footer.jsx';
-import './App.css';
+import React from "react";
+import Home from "./components/Home/Home.jsx";
+import Skills from "./components/Skills/Skills.jsx";
+import Services from "./components/Services/Services.jsx";
+import Projects from "./components/Projects/Projects.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import "./App.css";
 
 function App() {
   return (
     <main>
-      <Home/>
-      <Skills/>
-      <Services/>
-      <Projects/>
-      <Contact/>
-      <Footer/>
+      <Home />
+      <Skills />
+      <Services />
+      <Projects />
+      <Contact />
+      <Footer />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
